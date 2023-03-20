3f120e89d526f19ccd5548e85d2722cf6b1972dd
0xRar <isa.ebra@yahoo.com>
2023-03-20 12:45:30 +0300

    🍝 init code

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Oryx-Auto
 
 A simple recon automation script nothing special, runs scans & keep logs. 
+
+Q: i know my code is messy, don't like my 🍝 code?
+A: consider contributing, im happy to learn.
---
+++ oryx.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import subprocess
+import argparse
+
+from playsound import playsound
+from lolpython import lol_py
+
+
+def banner():
+    lol_py('┌─┐┬─┐┬ ┬─┐ ┬')
+    lol_py('│ │├┬┘└┬┘┌┴┬┘')
+    lol_py('└─┘┴└─ ┴ ┴ └─')
+    lol_py('# Recon Automation ~ mainly for webapp security testing')
+    lol_py('# By 0xRar ~ (0xrar.net)\n')
+
+
+def main():
+    examples = f'''Examples:
+    python3 {sys.argv[0]} -t hackerone.com
+
+    (For boot2root challenges)
+    python3 {sys.argv[0]} -c -t machine.htb
+    '''
+
+    parser = argparse.ArgumentParser(
+        epilog=examples,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    parser.add_argument(
+        "-t",
+        dest="target",
+        help="target domain/ip",
+        type=str,
+        required=True
+    )
+
+    if len(sys.argv) < 2:
+        parser.print_usage()
+        sys.exit(1)
+
+    args = parser.parse_args()
+    target = args.target
+
+    # Commands to run on target
+    nmap = f'nmap -p- -sC -sV -Pn -T4 --min-rate=10000 {target}|tee nmap.log'
+    subfinder = f'subfinder -nW -d {target}| tee sub-domains.log'
+    arjun = f'arjun -u http://{target}/ -w /usr/share/wordlists/SecLists/Discovery/Web-Content/burp-parameter-names.txt| tee parameters.log'
+    gobuster = f'gobuster dir -u http://{target}/ -w /usr/share/wordlists/directory-list-2.3-medium.txt -x .php,.html,.bak,.conf| tee dirs.log'
+    nuclei = f'nuclei -u http://{target}/ -as| tee nuclei.log'
+
+    commands = [nmap, subfinder, arjun, gobuster, nuclei]
+
+    # next: katana? naabu? 👀
+
+    print('# Will run nmap, subfinder, arjun, gobuster, nuclei')
+    print('# Please Wait...\n\n')
+
+    # Iterates through commands
+    for command in commands:
+        subprocess.run(command, shell=True)
+
+    # Run an alert when the recon finishes
+    playsound('./resources/recon_finished.mp3')
+
+
+if __name__ == "__main__":
+    banner()
+    main()
