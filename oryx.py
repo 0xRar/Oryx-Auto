9b3535611f10408a645f6e5513663d1dec74d78a
0xRar <33517160+0xRar@users.noreply.github.com>
2023-03-27 18:39:06 +0300

    Update oryx.py

--- oryx.py
+++ oryx.py
@@ -5,21 +5,28 @@ import sys
 import subprocess
 import argparse
 
-from playsound import playsound
 from lolpython import lol_py
 
 
 def banner():
+    lol_py('=======================================================')
     lol_py('┌─┐┬─┐┬ ┬─┐ ┬')
     lol_py('│ │├┬┘└┬┘┌┴┬┘')
     lol_py('└─┘┴└─ ┴ ┴ └─')
-    lol_py('# Recon Automation ~ mainly for webapp security testing')
-    lol_py('# By 0xRar ~ (0xrar.net)\n')
+    lol_py('# Recon Automation ~ for webapp security testing')
+    lol_py('# By 0xRar ~ (0xrar.net)')
+    lol_py('=======================================================')
 
 
 def main():
+    examples = f'''
+    examples:
+    python3 {sys.argv[0]} -t hackerone.com
+    python3 {sys.argv[0]} -c -t machine.htb
+    '''
     parser = argparse.ArgumentParser(
-        epilog=f'\tExample: \r\npython3 {sys.argv[0]} -t hackerone.com'
+        epilog=examples,
+        formatter_class=argparse.RawTextHelpFormatter
     )
 
     parser.add_argument(
@@ -30,33 +37,48 @@ def main():
         required=True
     )
 
+    parser.add_argument(
+        "-c",
+        dest="ctf",
+        help="used for ctfs/boot2root",
+        required=False,
+        action='store_true'
+    )
+
     if len(sys.argv) < 2:
         parser.print_usage()
         sys.exit(1)
 
     args = parser.parse_args()
     target = args.target
+    ctf = args.ctf
 
-    # Commands to run on target
-    nmap = f'nmap -p- -sC -sV -Pn -T4 --min-rate=10000 {target}|tee nmap.log'
-    subfinder = f'subfinder -nW -d {target}| tee sub-domains.log'
-    arjun = f'arjun -u http://{target}/ -w /usr/share/wordlists/SecLists/Discovery/Web-Content/burp-parameter-names.txt| tee parameters.log'
-    gobuster = f'gobuster dir -u http://{target}/ -w /usr/share/wordlists/directory-list-2.3-medium.txt -x .php,.html,.bak,.conf| tee dirs.log'
-    nuclei = f'nuclei -u http://{target}/ -as| tee nuclei.log'
+    # Wordlists
+    params = '/usr/share/wordlists/SecLists/Discovery/Web-Content/burp-parameter-names.txt'
+    dirs_files = '/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -x .php,.js,.html,.bak,.xml'
 
-    commands = [nmap, subfinder, arjun, gobuster, nuclei]
+    # Commands
+    nmap = f'nmap -p- -sC -sV -Pn -T4 --min-rate=10000 {target}| tee recon/nmap.log'
+    subfinder = f'subfinder -nW -d {target}| tee recon/sub-domains.log'
+    arjun = f'arjun --stable -u http://{target}/ -w {params}| tee recon/parameters.log'
+    gobuster = f'gobuster dir -u http://{target}/ -w {dirs_files}| tee recon/dirs.log'
+    nuclei = f'nuclei -u http://{target}/ -as| tee recon/nuclei.log'
 
-    # next: katana? naabu? 👀
+    print('# Please Wait 😄...\n')
 
-    print('# Will run nmap, subfinder, arjun, gobuster, nuclei')
-    print('# Please Wait...\n\n')
+    # Creating a recon directory to store the results
+    subprocess.run('mkdir recon', shell=True)
 
     # Iterates through commands
+    commands = [nmap, subfinder, arjun, gobuster, nuclei]
+
+    if target and ctf:
+        del commands[1]
+
     for command in commands:
         subprocess.run(command, shell=True)
 
-    # Run an alert when the recon finishes
-    playsound('./resources/recon_finished.mp3')
+    print('\nHappy Hacking 😄')
 
 
 if __name__ == "__main__":
